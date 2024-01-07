d9ba0824ab5e85a4ffe4d721e0557f68a05b1c5a
Hamisa Rizgallah <hamisa.rizgallah@student.moringaschool.com>
2024-01-07 16:43:48 +0300

    Completed Assignment

---
+++ index.js
@@ -0,0 +1,26 @@
+function add(a, b){
+    return a + b;
+}
+function subtract(a, b){
+    return a - b;
+}
+function multiply(a, b){
+    return a * b;
+}
+function divide(a, b){
+    return a / b;
+}
+let number = 10;
+function increment(number){
+    return (number+=1);
+}
+let n = 10;
+function decrement(n){
+    return (n-=1);
+}
+function makeInt(n){
+    return parseInt(n, 10);
+}
+function preserveDecimal(n){
+    return parseFloat(n);
+}
